23082ce68166f48645da2b77e56657bf400d51ca
Yontemoon <131310574+Yontemoon@users.noreply.github.com>
2023-07-27 15:07:48 -0700

    Added morgan and completed part3

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+app.use(express.json()) //LOOK INTO THIS A LITTLE MORE...
+
 
 var persons = [
     { 
@@ -24,6 +27,13 @@ var persons = [
     }
 ]
 
+
+morgan.token('object', function (req, res) { 
+  return `${JSON.stringify(req.body)}`
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
+
 app.get("/", (request, response) => {
   response.send(`<h1>THIS IS A PHONEBOOK TEST</h2> </br>
                 <p>Phonebook has info for ${persons.length} people.</p>
@@ -43,7 +53,51 @@ app.get("/api/persons/:id", (request, response) => {
     response.send("<h1>DOES NOT EXIST</h1>")
     response.status(404).end();
   }
+})
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end();
+})
+
+const generateId = () => {
+  const maxNum = Math.max(...persons.map(person => person.id));
+  return maxNum + 1;
+}
+
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  
+  if (!body.name || !body.number) {
+    return response
+      .status(400)
+      .json(
+        {
+          error: "content missing..."
+        }
+      )
+  }
+
+  persons.find(person => {
+    if (person.name === body.name) {
+      return response
+        .status(400)
+        .json({
+          error: "No duplicate names allowed..."
+        })
+    }
+  })
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+  
+  persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001;
